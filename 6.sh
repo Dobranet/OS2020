ae481341027ea05131f0965ae972d1ec20408b63
Dobranet <71813612+Dobranet@users.noreply.github.com>
2020-12-10 18:38:11 +0400

    Delete 6.sh

--- 6.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-	ps aux | tail -n+2 | sort -rnk4 > n
-	line=$(head -n 1 n)
-	pid=`echo $line | cut -d ' ' -f 2`
-	cat /proc/$pid/status > n
-	line=$(head -n 1 n)
-	echo $line > answ6
-	rm n
-exit 1
